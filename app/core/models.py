4728b9e9ef0727123870cc003d7a5302296acfd5
SheikhFahan <business.fahan@gmail.com>
2024-03-17 11:54:34 +0530

    custom user model

--- app/core/models.py
+++ app/core/models.py
@@ -1,3 +1,45 @@
-from django.db import models  # noqa
+"""database models"""
 
-# Create your models here.
+
+from django.db import models
+from django.contrib.auth.models import (
+    AbstractBaseUser,  # func for the authentication
+    PermissionsMixin,   # func for the permissions & fields
+    BaseUserManager,
+)
+
+
+class UserManager(BaseUserManager):
+    """manager for users"""
+
+    def create_user(self, email, password=None, **extrafields):
+        """creates saves and returns a new user"""
+        if not email:
+            raise ValueError("User must have an email")
+        user = self.model(email=self.normalize_email(email), **extrafields)
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+    def create_superuser(self, email, password=None, **extrafields):
+        """create and return a new superuser"""
+        user = self.create_user(email, password)
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
+
+        return user
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    """Create user model"""
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email'
+    # change the default authentication field to email
